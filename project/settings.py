37e5bff50506a5a49fb8302b57e34bd2039517ea
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-26 11:38:22 +0300

    перенос в окружение

--- project/settings.py
+++ project/settings.py
@@ -6,18 +6,18 @@ env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': env("ENGINE"),
-        'HOST': env("HOST"),
-        'PORT': env("PORT"),
-        'NAME': env("NAME"),
-        'USER': env("USER"),
-        'PASSWORD': env("PASSWORD"),
+        'ENGINE': env("DB_ENGINE"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG")
 
